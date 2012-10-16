ce40a8faa55bb793ea6910d01b544e9bd4ad9905
DwiraSurvivor <djenuar@yahoo.co.id>
2012-10-16 11:07:10 -0700

    Baca saya sebelum menggunakan

--- config/fungsi_badword.php
+++
@@ -1,9 +0,0 @@
-<?php
-function sensor($teks){
-    $w = mysql_query("SELECT * FROM katajelek");
-    while ($r = mysql_fetch_array($w)){
-        $teks = str_replace($r['kata'], $r['ganti'], $teks);       
-    }
-    return $teks;
-}  
-?>
